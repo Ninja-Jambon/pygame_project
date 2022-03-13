c43d042ddd040579b176de9b1fd53fa1f61b6f44
Lukian Leizour <lulul7018@gmail.com>
2022-03-13 18:00:03 +0100

    commit

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ HOME_FONT = pygame.font.SysFont('comicsans', 40)
 
 WIDTH = 900
 HEIGHT = 500
+BOY_WIDTH = 50
+BOY_HEIGHT = 50
 
 FPS = 60
+
+VEL = 5
 
 BLACK = (0, 0, 0)
 CYAN = (0, 255, 255)
 WHITE = (255,255,255)
 
-RED_BOY = pygame.image.load(
-    os.path.join("Assets","red_sprite.png"))
-BLUE_BOY = pygame.image.load(
-    os.path.join("Assets","blue_sprite.png"))
+RED_BOY_IMAGE = pygame.image.load(os.path.join("Assets","red_sprite.png"))
+BLUE_BOY_IMAGE = pygame.image.load(os.path.join("Assets","blue_sprite.png"))
 
+RED_BOY = pygame.transform.scale(RED_BOY_IMAGE,(BOY_WIDTH, BOY_HEIGHT))
+BLUE_BOY = pygame.transform.scale(BLUE_BOY_IMAGE,(BOY_WIDTH, BOY_HEIGHT))
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("")
@@ -29,11 +33,28 @@ def home_window():
     WIN.blit(home_text, (WIDTH//2 - (home_text.get_width()//2), HEIGHT//2 - (home_text.get_height()//2)))
     pygame.display.update()
 
-def draw_window():
+def draw_window(red,blue):
     WIN.fill(CYAN)
+    WIN.blit(RED_BOY, (red.x,red.y))
+    WIN.blit(BLUE_BOY, (blue.x,blue.y))
     pygame.display.update()
 
+def blue_handle_movement(keys_pressed, blue):
+    if keys_pressed[pygame.K_q] and blue.x - VEL >= 0: #LEFT
+            blue.x -= VEL
+    if keys_pressed[pygame.K_d] and blue.x + VEL + blue.width <= WIDTH: #RIGHT
+            blue.x += VEL
+
+def red_handle_movement(keys_pressed, red):
+    if keys_pressed[pygame.K_LEFT] and red.x - VEL >= 0: #LEFT
+            red.x -= VEL
+    if keys_pressed[pygame.K_RIGHT] and red.x + VEL + red.width <= WIDTH: #RIGHT
+            red.x += VEL
+
 def main():
+    red = pygame.Rect(700, 300, BOY_WIDTH, BOY_HEIGHT)
+    blue = pygame.Rect(200, 300, BOY_WIDTH, BOY_HEIGHT)
+
     clock = pygame.time.Clock()
     run = True
     home = True
@@ -50,8 +71,9 @@ def main():
         if home == True:
             home_window()
         elif home == False:
-            draw_window()
-
+            draw_window(red, blue)
+        blue_handle_movement(keys_pressed, blue)
+        red_handle_movement(keys_pressed, red)
     pygame.quit()
 
 
